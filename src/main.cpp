13f5864b570ab4a30c6390fa8b7d4d8094bbba61
Hegedűs István András <hegedus.i.andris@gmail.com>
2025-09-30 23:17:41 +0200

    buzzer test

--- src/main.cpp
+++ src/main.cpp
@@ -5,44 +5,42 @@
 #include "esp_log.h"
 
 #define TAG "ESP32S3"
-#define DELAY pdMS_TO_TICKS(2000)
+#define DELAY pdMS_TO_TICKS(20)
 
 #define BUZZER_GPIO               21
 #define BUZZER_TIMER              LEDC_TIMER_0
 #define BUZZER_MODE               LEDC_LOW_SPEED_MODE
-#define BUZZER_CHANNEL            LEDC_CHANNEL_0
-#define BUZZER_DUTY_RES           LEDC_TIMER_13_BIT
-#define BUZZER_DUTY               4096
-#define BUZZER_FREQUENCY          442
+#define BUZZER_DUTY               6000
+#define BUZZER_START_FREQUENCY    8000
 
 static void buzzer_init() {
-    constexpr ledc_timer_config_t BUZZER_timer = {
+    static const ledc_timer_config_t buzzer_timer = {
         .speed_mode       = BUZZER_MODE,
-        .duty_resolution  = BUZZER_DUTY_RES,
+        .duty_resolution  = LEDC_TIMER_13_BIT,
         .timer_num        = BUZZER_TIMER,
-        .freq_hz          = BUZZER_FREQUENCY,
-        .clk_cfg          = LEDC_AUTO_CLK
+        .freq_hz          = BUZZER_START_FREQUENCY,
+        .clk_cfg          = LEDC_AUTO_CLK,
+        .deconfigure      = false
     };
-    ESP_ERROR_CHECK(ledc_timer_config(&BUZZER_timer));
+    ESP_ERROR_CHECK(ledc_timer_config(&buzzer_timer));
 
-    constexpr ledc_channel_config_t BUZZER_channel = {
+    static const ledc_channel_config_t buzzer_channel = {
         .gpio_num       = BUZZER_GPIO,
-        .speed_mode     = BUZZER_MODE,
-        .channel        = BUZZER_CHANNEL,
+        .speed_mode     = LEDC_LOW_SPEED_MODE,
+        .channel        = LEDC_CHANNEL_0,
         .intr_type      = LEDC_INTR_DISABLE,
-        .timer_sel      = BUZZER_TIMER,
-        .duty           = 0, // Set duty to 0%
-        .hpoint         = 0,
-        .sleep_mode     = LEDC_SLEEP_MODE_KEEP_ALIVE,
-        .flags          = {0}
+        .timer_sel      = LEDC_TIMER_0,
+        .duty           = BUZZER_DUTY,
+        .hpoint         = 0
     };
-    ESP_ERROR_CHECK(ledc_channel_config(&BUZZER_channel));
+    ESP_ERROR_CHECK(ledc_channel_config(&buzzer_channel));
 }
 
 extern "C" [[noreturn]] void app_main(void) {
     buzzer_init();
-    while (true) {
-        ESP_LOGI(TAG, "Playing sound on GPIO %n...", BUZZER_GPIO);
+    for (uint32_t freq = 20; freq < 10000; freq += 5) {
+        ledc_set_freq(BUZZER_MODE, BUZZER_TIMER, freq);
+        ESP_LOGI(TAG, "Playing sound on GPIO %d...", BUZZER_GPIO);
         vTaskDelay(DELAY);
     }
 }
